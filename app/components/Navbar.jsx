335e9fbec7d5ae1c8584ecf5cd79814464e6ea82
kalu john <kalu.johnagu@gmail.com>
2025-03-23 23:50:11 +0100

    navbar component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -16,35 +16,85 @@ const Navbar = () => {
       </a>
 
       {/* Hidden on small screens, flex on md+ */}
-      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm opacity-50'>
         <li>
-          <a href='#top'>Home</a>
+          <a className='font-Ovo' href='#top'>
+            Home
+          </a>
         </li>
         <li>
-          <a href='#about'>About</a>
+          <a className='font-Ovo' href='#about'>
+            About
+          </a>
         </li>
         <li>
-          <a href='#services'>Services</a>
+          <a className='font-Ovo' href='#services'>
+            Services
+          </a>
         </li>
         <li>
-          <a href='#work'>My Work</a>
+          <a className='font-Ovo' href='#work'>
+            My Work
+          </a>
         </li>
         <li>
-          <a href='#contact'>Contact me</a>
+          <a className='font-Ovo' href='#contact'>
+            Contact me
+          </a>
         </li>
       </ul>
 
-      <div className='flex items-center gap-2'>
-        <a href='#contact' className='flex items-center gap-1'>
+      <div className='flex items-center gap-4'>
+        <button>
+          <Image src={assets.moon_icon} className='w-6' alt='moon icon' />
+        </button>
+        <a
+          href='#contact'
+          className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'
+        >
           Contact
-          <Image
-            src={assets.arrow_icon}
-            width={12}
-            height={12}
-            alt='arrow icon'
-          />
+          <Image src={assets.arrow_icon} className='w-3' alt='arrow icon' />
         </a>
+        <button className='block md:hidden ml-3'>
+          <Image src={assets.menu_black} className='w-6' alt='menu black' />
+        </button>
       </div>
+      {/**  MOBILE MENU */}
+
+      <ul className='flex md:hidden flex-col gap-4 py-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+        <div className='absolute right-6 top-6 '>
+          <Image
+            src={assets.close_black}
+            alt='close'
+            className='w-5 cursor-pointer '
+          />
+        </div>
+        <li>
+          <a className='font-Ovo' href='#top'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a className='font-Ovo' href='#about'>
+            About
+          </a>
+        </li>
+        <li>
+          <a className='font-Ovo' href='#services'>
+            Services
+          </a>
+        </li>
+        <li>
+          <a className='font-Ovo' href='#work'>
+            My Work
+          </a>
+        </li>
+        <li>
+          <a className='font-Ovo' href='#contact'>
+            Contact me
+          </a>
+        </li>
+      </ul>
     </nav>
   )
 }
