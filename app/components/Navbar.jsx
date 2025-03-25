96b96402bbe0093454914afb6892c56a373add37
kalu john <kalu.johnagu@gmail.com>
2025-03-25 21:11:24 +0100

    navbar functionality

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,20 @@
+'use client'
 import { assets } from '@/assets/assets'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => !prev)
+    document.documentElement.classList.toggle('dark')
+  }
+const toggleMenu = () => {
+  setMenuOpen((prev) => !prev)
+}
+
   return (
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between bg-white shadow-md'>
       <a href='#top'>
@@ -15,7 +27,7 @@ const Navbar = () => {
         />
       </a>
 
-      {/* Hidden on small screens, flex on md+ */}
+      {/* Desktop Menu */}
       <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm opacity-50'>
         <li>
           <a className='font-Ovo' href='#top'>
@@ -45,7 +57,7 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-4'>
-        <button>
+        <button onClick={toggleDarkMode}>
           <Image src={assets.moon_icon} className='w-6' alt='moon icon' />
         </button>
         <a
@@ -55,18 +67,22 @@ const Navbar = () => {
           Contact
           <Image src={assets.arrow_icon} className='w-3' alt='arrow icon' />
         </a>
-        <button className='block md:hidden ml-3'>
+        <button className='block md:hidden ml-3' onClick={toggleMenu}>
           <Image src={assets.menu_black} className='w-6' alt='menu black' />
         </button>
       </div>
-      {/**  MOBILE MENU */}
 
-      <ul className='flex md:hidden flex-col gap-4 py-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
-        <div className='absolute right-6 top-6 '>
+      {/* Mobile Menu */}
+      <ul
+        className={`flex md:hidden flex-col gap-4 py-10 fixed ${
+          menuOpen ? 'right-0' : '-right-64'
+        } top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500`}
+      >
+        <div className='absolute right-6 top-6' onClick={toggleMenu}>
           <Image
             src={assets.close_black}
             alt='close'
-            className='w-5 cursor-pointer '
+            className='w-5 cursor-pointer'
           />
         </div>
         <li>
